413b9d870ca28bf7ee6e9132d831e95cd23d55bd
BoudaDols <dolsombouda@gmail.com>
2025-10-06 23:14:47 -0400

    Message Features

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -14,10 +14,19 @@ class Message extends Model
         'media_filename',
         'media_size',
         'read_at',
+        'reply_to_id',
+        'is_edited',
+        'edited_at',
+        'is_deleted',
+        'deleted_at',
     ];
 
     protected $casts = [
         'read_at' => 'datetime',
+        'edited_at' => 'datetime',
+        'deleted_at' => 'datetime',
+        'is_edited' => 'boolean',
+        'is_deleted' => 'boolean',
     ];
 
     public function chatRoom()
@@ -28,5 +37,25 @@ class Message extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function reactions()
+    {
+        return $this->hasMany(MessageReaction::class);
+    }
+
+    public function replyTo()
+    {
+        return $this->belongsTo(Message::class, 'reply_to_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Message::class, 'reply_to_id');
+    }
+
+    public function scopeNotDeleted($query)
+    {
+        return $query->where('is_deleted', false);
     }
 }
